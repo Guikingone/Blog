b6f78f40ec0664b46ec5b00a729a0a236753a12c
Guillaume Loulier <guillaume@mbpdeguillaume-1.home>
2017-05-12 09:34:29 +0200

    [ADD + INIT](Core)[!M] Core|Initialized + Logic|Started

---
+++ etc/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Router
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class Router
+{
+    public function execute()
+    {
+        if ($_SERVER['REQUEST_URI'] === '/') {
+
+        }
+    }
+}
---
+++ src/Action/HomeAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core\Action;
+
+/**
+ * Class HomeAction
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class HomeAction
+{
+    public function __construct()
+    {
+
+    }
+
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
