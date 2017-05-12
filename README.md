2294a76a0315a7ae3b225df046a9d9f5e127cd25
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-05-12 14:56:40 +0200

    [ADD](Core + Performances)[!M] Core|Progress on services + Performances tests|Progress

---
+++ README.md
@@ -0,0 +1,5 @@
+## Suivi
+
+Insight 
+
+[![SensioLabsInsight](https://insight.sensiolabs.com/projects/1bbe11bc-ecd9-4973-bb81-2cb7bea15169/big.png)](https://insight.sensiolabs.com/projects/1bbe11bc-ecd9-4973-bb81-2cb7bea15169)
---
+++ tests/Performances/Actions/HomeActionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Performances\Actions;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class HomeActionTest
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class HomeActionTest extends TestCase
+{
+
+}
