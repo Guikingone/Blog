7ae022cafa6c05ada1650538f7460fa4ef706895
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-05-13 09:29:25 +0200

    [FIX](Kernel)[!M] Kernel|Actions loader + To test

--- etc/Kernel.php
+++ etc/Kernel.php
@@ -22,6 +22,12 @@ class Kernel
     private $parameters;
 
     /** @var array */
+    private $actions;
+
+    /** @var array */
+    private $responders;
+
+    /** @var array */
     private $services;
 
     /**
@@ -55,6 +61,53 @@ class Kernel
         $this->parameters = require __DIR__ . '/config/parameters.php';
     }
 
+    /**
+     * Allow to load every Actions registered into the Action folder.
+     *
+     * @throws \ReflectionException
+     */
+    public function loadActions()
+    {
+        $finder = new Finder();
+        try {
+            $finder->in(__DIR__.'../src/Action')->files()->name('*Action.php');
+        } catch(\InvalidArgumentException $e) {
+            $e->getMessage();
+        }
+
+        foreach ($finder as $file) {
+            $class = new \ReflectionClass($file->getFilename());
+            $args = $class->getConstructor()->getParameters();
+
+            foreach ($args as $parameter) {
+                if (array_key_exists($parameter, $this->responders)) {
+                    $argument = new $this->responders[$parameter];
+                    $this->actions[$class->getName()] = $class->newInstanceArgs($argument);
+                }
+            }
+        }
+    }
+
+    public function loadResponders()
+    {
+        $finder = new Finder();
+        try {
+            $finder->in(__DIR__.'../src/Responders')->files()->name('*Responder.php');
+        } catch(\InvalidArgumentException $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function loadManagers()
+    {
+        $finder = new Finder();
+        try {
+            $finder->in(__DIR__.'../src/Managers')->files()->name('*Manager.php');
+        } catch(\InvalidArgumentException $e) {
+            $e->getMessage();
+        }
+    }
+
     public function loadServices()
     {
         $this->services = require __DIR__ . '/config/services.php';
@@ -70,18 +123,6 @@ class Kernel
             $loader = new \Twig_Loader_Filesystem([$this->parameters['views_folder']]);
             return new \Twig_Environment($loader);
         };
-    }
-
-    public function loadActions()
-    {
-        $classes = [];
-
-        $finder = new Finder();
-        try {
-            $finder->in(__DIR__.'../src/Action')->files()->name('*Action.php');
-        } catch(\InvalidArgumentException $e) {
-            $e->getMessage();
-        }
     }
 
     /**
